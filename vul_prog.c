ce67c951c65a87044327b3f3600a0a6bc0bb326c
Allen <allenclark@u.boisestate.edu>
2019-04-05 01:39:03 -0600

    changed int a to pointer

--- vul_prog.c
+++ vul_prog.c
@@ -11,27 +11,27 @@ int main(int argc, char *argv[])
     char user_input[100];
     int *secret;
     int int_input;
-    int a, b, c, d; /* other variables, not used here.*/
+    int b, c, d; /* other variables, not used here.*/
 
 	// we need to take up space so that we have a nice offset
 	// to easily find the desired address
-	a = (int *) malloc(2 * sizeof(int));
+	int *a = (int *) malloc(2 * sizeof(int));
 
 	/* The secret value is stored on the heap */
     secret = (int *) malloc(2*sizeof(int));
     /* getting the secret */
     secret[0] = SECRET1; secret[1] = SECRET2;
-    
+
     printf("The variable secret's address is 0x%8x (on stack)\n", (unsigned int)&secret);
     printf("The variable secret's value is 0x%8x (on heap)\n", (unsigned int)secret);
     printf("secret[0]'s address is 0x%8x (on heap)\n", (unsigned int)&secret[0]);
     printf("secret[1]'s address is 0x%8x (on heap)\n", (unsigned int)&secret[1]);
-    
+
 //    printf("Please enter a decimal integer\n");
 //    scanf("%d", &int_input);  /* getting an input from user */
     printf("Please enter a string\n");
     scanf("%s", user_input); /* getting a string from user */
-    
+
     /* Vulnerable place */
     printf(user_input);
     printf("\n");
